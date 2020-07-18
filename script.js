77b13b2545eb4651347845df08c70f28cf5e2d34
Mandri905 <62116946+Mandri905@users.noreply.github.com>
2020-07-18 23:13:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 let urlStr = window.location.search;
 const paramName = 'username';
+
 let userName =  urlStr.substr(urlStr.indexOf(paramName, 0) + paramName.length + 1);
+
+if (userName.length === 0) {
+  userName = 'Mandri905'
+};
+
 let urlGit = 'https://api.github.com/users/' + userName;
 
 fetch(urlGit)
-	.then(res => res.json())
-	.then(json => {
-		if (json.name !== undefined) {
-			var h2 = document.createElement('h2');
-			h2.innerHTML = '<a href=' + json.html_url + '>' + json.name + '</a>';
-			document.body.appendChild(h2);
+  .then (resStatus => {
+    if (resStatus.status == 404) {
+	  let div = document.createElement('div');
+	  div.innerHTML = 'Информация о пользователе ' + userName + ' не доступна';
+	  document.body.appendChild(div);	
+  	}
+  	else { console.log('1');
+  	  fetch(urlGit)
+  	    .then(res => res.json())
+  		.then(json => {
+		  let h2 = document.createElement('h2');
+		  h2.innerHTML = '<a href=' + json.html_url + '>' + json.name + '</a>';
+		  document.body.appendChild(h2);
 
-			var div = document.createElement('div');
-			div.innerHTML = json.bio;
-			document.body.appendChild(div);
-			
-			var img = document.createElement('img');
-			img.src = json.avatar_url;
-			document.body.appendChild(img);
-		}
-		else {
-			var div = document.createElement('div');
-			div.innerHTML = 'Информация о пользователе не доступна';
-			document.body.appendChild(div);
-		}
-	});
+		  let div = document.createElement('div');
+		  div.innerHTML = json.bio;
+		  document.body.appendChild(div);
+		
+		  let img = document.createElement('img');
+		  img.src = json.avatar_url;
+		  document.body.appendChild(img);	
+		})
+  	}
+  })
+  .catch (err => {
+    let div = document.createElement('div');
+  	div.innerHTML = 'Ошибка: ' + err;
+	document.body.appendChild(div);
+  });
