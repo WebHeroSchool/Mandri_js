3c03474982b05f2d871ba3a85c9369972d353b8d
Mandri905 <62116946+Mandri905@users.noreply.github.com>
2020-07-19 15:15:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,29 +10,31 @@ if (userName.length === 0) {
 let urlGit = 'https://api.github.com/users/' + userName;
 
 fetch(urlGit)
-  .then (resStatus => {
-    if (resStatus.status == 404) {
+  .then (res => {
+    if (res.status == 404) {
 	  let div = document.createElement('div');
 	  div.innerHTML = 'Информация о пользователе ' + userName + ' не доступна';
-	  document.body.appendChild(div);	
+	  document.body.appendChild(div)
   	}
-  	else { console.log('1');
-  	  fetch(urlGit)
-  	    .then(res => res.json())
-  		.then(json => {
-		  let h2 = document.createElement('h2');
-		  h2.innerHTML = '<a href=' + json.html_url + '>' + json.name + '</a>';
-		  document.body.appendChild(h2);
+  	else { 
+  	  return (res.json())	
+  	}
+  })
+  .then (json => {
+  	let user = json.name;
+  	if (user === null) user = userName;
 
-		  let div = document.createElement('div');
-		  div.innerHTML = json.bio;
-		  document.body.appendChild(div);
-		
-		  let img = document.createElement('img');
-		  img.src = json.avatar_url;
-		  document.body.appendChild(img);	
-		})
-  	}
+  	let h2 = document.createElement('h2');
+  	h2.innerHTML = '<a href=' + json.html_url + '>' + user + '</a>';
+  	document.body.appendChild(h2);
+
+  	let div = document.createElement('div');
+  	div.innerHTML = json.bio;
+ 	document.body.appendChild(div);
+
+  	let img = document.createElement('img');
+  	img.src = json.avatar_url;
+	document.body.appendChild(img);
   })
   .catch (err => {
     let div = document.createElement('div');
